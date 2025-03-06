76df423d551ef8d9d6199f536d4a9692b26c1705
kmindev <kangkm0324@gmail.com>
2025-03-06 15:56:07 +0900

    boot 3.4.3 프로젝트 생성

---
+++ src/main/java/com/example/BootInfluxdbApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootInfluxdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootInfluxdbApplication.class, args);
+	}
+
+}
